2035c3d79f1125e6f1736a7a36a12ebb005f6130
nabeeltaariq <nabeeltaariq@gmail.com>
2021-01-12 17:12:22 +0500

    first commit created select option

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Select, MenuItem } from '@material-ui/core'
+import { FormControl } from '@material-ui/core'
+import './App.css'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  //state is something that sets value
+  //https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2, //UK,USA
+          }))
+
+          setCountries(countries)
+        })
+    }
+    getCountriesData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app__header'>
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined' value='abc'>
+            <MenuItem value='worlwide'>Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select input down field */}
+      {/* infoBox */}
+      {/* infoBoxes */}
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
